5d06d1f045c3ef3e2b94c3871fc1c3bc30b7eff9
Daniel Tedman <dbtedman@gmail.com>
2022-12-20 21:35:41 +1000

    Begin scaffolding resource sections

---
+++ ui/components/AccretionResourceSection/AccretionResourceSection.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts"></script>
+
+<template>
+    <section class="container">
+        <h1>Section</h1>
+        <div>
+            <slot />
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+@use "sass:math";
+@import "../../theme/size";
+@import "../../theme/colour";
+
+.container {
+    padding: math.div($grid, 2) $grid;
+    margin-top: $grid;
+    margin-bottom: $grid;
+    border: 1px solid darken($light-background, 4);
+    @media (prefers-color-scheme: dark) {
+        border: 1px solid darken($dark-background, 4);
+    }
+
+    h1 {
+        margin: 0;
+        padding: 0;
+        font-size: 1.2rem;
+        font-weight: 600;
+    }
+}
+</style>
--- ui/views/HomeView.vue
+++ ui/views/HomeView.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
 import AccretionSearch from "@/components/AccretionSearch/AccretionSearch.vue";
+import AccretionResourceSection from "@/components/AccretionResourceSection/AccretionResourceSection.vue";
 </script>
 
 <template>
     <AccretionSearch />
+    <AccretionResourceSection>One</AccretionResourceSection>
+    <AccretionResourceSection>Two</AccretionResourceSection>
+    <AccretionResourceSection>Three</AccretionResourceSection>
+    <AccretionResourceSection>Four</AccretionResourceSection>
 </template>
 
 <style lang="scss" scoped></style>
