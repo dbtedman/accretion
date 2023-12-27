7939e56877966f3074092d5ff489e1491e224b10
Daniel Tedman <dbtedman@gmail.com>
2023-12-27 19:21:26 +1000

    Fix focus of tests on result rather than intermediary errors

--- event/event_test.go
+++ event/event_test.go
@@ -10,12 +10,10 @@ import (
 func TestApplicationStartupPublishesEvent(t *testing.T) {
 	var log event.Loggable = &event.InMemoryLog{}
 	app := application.NewApplication(log)
-	err := app.Start()
-	assert.Nil(t, err, "Application startup should not return error.")
+	_ = app.Start()
 
 	anEvent, err := log.Latest()
 
-	assert.NotNil(t, anEvent, "A non null event should be popped.")
 	assert.Equal(t, anEvent.Kind(), "application_startup")
 	assert.Nil(t, err, "A nil error message should be returned.")
 }
