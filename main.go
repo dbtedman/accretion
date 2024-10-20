5b23f6730a50b7562599265d3f0add5ab0631113
Daniel Tedman <dbtedman@gmail.com>
2024-10-20 15:40:17 +1000

    Init

--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import "log"
 
+// version, commit, and date are populated during build
+var (
+	version = "latest"
+	commit  = "n/a"
+	date    = "n/a"
+)
+
 func main() {
 	log.Println("Hello, World!")
 }
